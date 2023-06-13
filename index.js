ff5c03b0e0eee9db80a17d54955bcf339a5017b3
Daniel <dwiest4@gmail.com>
2023-06-13 09:06:47 -0700

    Initial commit. Code copied from previous replit repo.
     On branch main
     Your branch is up to date with 'upstream/main'.
    
     Changes to be committed:
    	modified:   index.js
    	modified:   package.json
    	modified:   views/index.html

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer({dest:'uploads/'})
 
 var app = express();
 
@@ -18,3 +20,18 @@ const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+
+app.post(
+  '/api/fileanalyse',
+  upload.single('upfile'),
+  handleFileUpload
+)
+
+function handleFileUpload (req, res, next) {
+    res.send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+}
